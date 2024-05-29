8321c90c370b0afaa5a44b7260693d9286eb503f
shraddhayadav24 <slyadav240901@gmail.com>
2024-05-29 18:07:47 +0530

    Ability to manage employee wage of multiple companies

--- Employeewage.java
+++ Employeewage.java
@@ -1,32 +1,66 @@
 package com.employeewage.uc;
-public class EmployeeWage {
-	 
+
+class CompanyEmpWage{
 	private String CompanyName;
-	private int workinghourperday;
-	private int workinghourperrate;
 	private double totalEmpWage;
 	
-	public EmployeeWage(String CompanyName) {
+	public CompanyEmpWage(String CompanyName) {
 		this.CompanyName=CompanyName;
 		this.totalEmpWage=0.0;
 	}
 	
-	public double CalculateEmpWage(int workinghourperday, int workinghourperrate) {
-		double wage= workinghourperday*workinghourperrate;
-		totalEmpWage = totalEmpWage+wage;
-		return wage;
+	public String getCompanyName() {
+		return CompanyName;
 	}
 	
 	public double gettotalEmpWage() {
 		return totalEmpWage;
 	}
 	
-	
-	public static void main(String[] args) {
-		EmployeeWage employeeWage=new EmployeeWage("TCS");
-		double company1 = employeeWage.CalculateEmpWage(8, 20);
-		System.out.println(company1);
+	public double settotalEmpWage(double totalEmpWage) {
+		return this.totalEmpWage=totalEmpWage;
 	}
-	
-	
 }
+
+
+public class EmployeeWage {
+	 private CompanyEmpWage[] companyEmpWages;
+	 private int numofCompanies;
+	 
+	 public EmployeeWage() {
+		 companyEmpWages =new CompanyEmpWage[5];
+		 numofCompanies=0;
+	 }
+	 
+	 public void addCompany(String CompanyName) {
+		 companyEmpWages[numofCompanies]= new CompanyEmpWage(CompanyName);
+		 numofCompanies++;
+	 }
+	 
+	 public void CalculateEmpWage(int companyindex, int workhourperday, int workhourperrate) {
+		 double wage= workhourperday * workhourperrate;
+		 companyEmpWages[companyindex].settotalEmpWage(companyEmpWages[companyindex].gettotalEmpWage() + wage);
+		 
+	 }
+	 public double gettotalEmpWage(int companyIndex) {
+	        return companyEmpWages[companyIndex].gettotalEmpWage();
+	    }
+	
+	 public static void main(String[] args) {
+	        EmployeeWage empWageBuilder = new EmployeeWage();
+	        empWageBuilder.addCompany("TCS");
+	        empWageBuilder.addCompany("Wipro");
+
+	        
+	        empWageBuilder.CalculateEmpWage(0, 40, 15);
+	        empWageBuilder.CalculateEmpWage(0, 30, 20);
+	        System.out.println("Total wage for " + empWageBuilder.companyEmpWages[0].getCompanyName() + ": " + empWageBuilder.gettotalEmpWage(0));
+
+	        
+	        empWageBuilder.CalculateEmpWage(1, 45, 18);
+	        empWageBuilder.CalculateEmpWage(1, 35, 22);
+	        System.out.println("Total wage for " + empWageBuilder.companyEmpWages[1].getCompanyName() + ": " + empWageBuilder.gettotalEmpWage(1));
+	    }
+	}
+
+
