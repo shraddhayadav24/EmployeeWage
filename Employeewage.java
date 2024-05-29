ffbacad2d35ef2c2e7a9234431753409ed367d8a
shraddhayadav24 <slyadav240901@gmail.com>
2024-05-29 11:35:48 +0530

    Total Employeewage for each Company

--- Employeewage.java
+++ Employeewage.java
@@ -1,28 +1,32 @@
 package com.employeewage.uc;
-public class EmployeeWage{
-private double empwageperhour;
-private int workinghoursperday;
-private int workingdayspermonth;
-
-
-public EmployeeWage(double empwageperhour, int workinghoursperday, int workingdayspermonth) {
-	this.empwageperhour=empwageperhour;
-	this.workingdayspermonth=workingdayspermonth;
-	this.workinghoursperday=workinghoursperday;
-}
-
-public static double CalculateEmpwageCompanies(double empwageperhour, int workinghoursperday, int workingdayspermonth) {
-	int totalworkinghours= workinghoursperday * workingdayspermonth;
-	double totalWage= totalworkinghours * empwageperhour;
-	return totalWage;
-}
-
-public static void main(String[] args) {
-	double Company1 =EmployeeWage.CalculateEmpwageCompanies(15.0, 8, 30);
-	System.out.println("The total EmpWage for Cpmpany 1:  " + Company1);
+public class EmployeeWage {
+	 
+	private String CompanyName;
+	private int workinghourperday;
+	private int workinghourperrate;
+	private double totalEmpWage;
 	
-	double Company2 =EmployeeWage.CalculateEmpwageCompanies(10.0, 8, 20);
-	System.out.println("The total EmpWage for Cpmpany 2:  "+Company2);
+	public EmployeeWage(String CompanyName) {
+		this.CompanyName=CompanyName;
+		this.totalEmpWage=0.0;
+	}
 	
-}
+	public double CalculateEmpWage(int workinghourperday, int workinghourperrate) {
+		double wage= workinghourperday*workinghourperrate;
+		totalEmpWage = totalEmpWage+wage;
+		return wage;
+	}
+	
+	public double gettotalEmpWage() {
+		return totalEmpWage;
+	}
+	
+	
+	public static void main(String[] args) {
+		EmployeeWage employeeWage=new EmployeeWage("TCS");
+		double company1 = employeeWage.CalculateEmpWage(8, 20);
+		System.out.println(company1);
+	}
+	
+	
 }
